00dd0aca5937eb4bc13e47eccfd957a55aed32e5
Caia Chuang <caiachuang@gmail.com>
2023-12-06 14:25:33 -0800

    add read operation

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 # Import libraries
-
+from flask import Flask, request, url_for, redirect, render_template
 # Instantiate Flask functionality
+app = Flask(__name__)
 
 # Sample data
-
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 # Read operation
+@app.route('/')
+def get_transactions():
+    return render_template('transactions.html', transactions=transactions)
 
 # Create operation
 
@@ -13,4 +21,3 @@
 # Delete operation
 
 # Run the Flask app
-    
