ee0f19edf6321dfc9dcfa16762da26005011030a
Caia Chuang <caiachuang@gmail.com>
2023-12-06 14:35:54 -0800

    add update operation

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def add_transaction():
     if request.method == 'GET':
         return render_template('form.html')
     if request.method == 'POST':
-        transaction ={
+        transaction = {
             'id': len(transactions) + 1,
             'date': request.form['date'],
             'amount': float(request.form['amount']),
@@ -29,6 +29,24 @@ def add_transaction():
         return redirect(url_for('get_transactions'))
 
 # Update operation
+@app.route('/edit/<int:transaction_id>', methods = ['GET', 'POST'])
+def edit_transaction(transaction_id):
+    if request.method == 'GET':
+        for transaction in transactions:
+            if transaction['id'] == transaction_id:
+                return render_template('edit.htm', transaction=transaction)
+
+    if request.method == 'POST':
+        date = request.form['date']
+        amount = float(request.form['amount'])
+
+        for transaction in transactions:
+            if transaction['id'] == transaction_id:
+                transaction['date'] = date
+                transaction['amount'] = amount
+                break
+
+        return redirect(url_for('get_transactions'))
 
 # Delete operation
 
